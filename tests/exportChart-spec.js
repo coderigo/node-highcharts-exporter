1502e2059c05f17fc4fe35caa0d0e6dbac591e3b
Rodrigo Martell <rodrigo.martell@gmail.com>
2014-01-16 16:05:00 +1100

    Include tests.

--- tests/exportChart-spec.js
+++ tests/exportChart-spec.js
@@ -1,7 +1,86 @@
-var nhe = require('../lib/node-highcharts-exporter/main.js');
+var nhe               = require('../lib/node-highcharts-exporter/main.js'),
+    fs                = require('fs'),
+    chart1SVG         = fs.readFileSync(__dirname + '/chart1.svg'),
+    rmdir             = require('rimraf'),
+    testProcessingDir = './.' + '/test_exports';
 
-describe('Check that it exports', function () {
-    it("should export stuff", function(){
-        expect(10).toBe(10);
+describe('Config options', function(){
+
+    it('should complain when given an invalid config option to set', function () {
+        expect(function(){nhe.config.set('notAnOption', 'not an option value')}).toThrow();
     });
+
+    it('should allow setting of processing directory', function () {
+        var defaultProcessingDir = './.' + '/highcharts_exports';
+        expect(nhe.config.get().processingDir).toBe(defaultProcessingDir);
+
+        nhe.config.set('processingDir', testProcessingDir);
+        expect(nhe.config.get().processingDir).toBe(testProcessingDir);
+    });
+});
+
+describe('Chart exporting', function () {
+
+    /**
+     * Tests exporting to supported formats as they come 
+     * @param  {object}  format An export test format
+     * @return {boolean}        Test chartExport synchronous version
+     */
+    var testExport = function(format, sync){
+
+        it('should return ' + format.extension +' when requested ' + format.extension, function(done){
+
+            var chartName             = 'export_to_' + format.extension,
+                chartNameAndExtension = chartName + '.' + format.extension,
+                highchartsRequest     = { // The mock POSTed request from Highcharts
+                    type     : format.mediaType,
+                    filename : chartName,
+                    scale    : 2,
+                    svg      : chart1SVG
+                };
+
+            nhe.exportChart(highchartsRequest, function(error, exportedChartInfo){
+
+                expect(error).toBe(null);
+
+                expect(exportedChartInfo['parentDir']).toBeDefined();
+                expect(fs.existsSync(exportedChartInfo['parentDir'])).toBe(true);
+                expect(exportedChartInfo['filePath']).toBeDefined();
+                expect(fs.existsSync(exportedChartInfo['filePath'])).toBe(true);
+
+                expect(exportedChartInfo['type']).toBe(format.extension);
+                expect(exportedChartInfo['fileName']).toBe(chartName);
+                expect(exportedChartInfo['file']).toBe(chartNameAndExtension);
+
+                expect(exportedChartInfo['parentDir']).toBeDefined();
+
+                // Done sync as tests may run too quickly and thus land in
+                // the same processing directory as its name comes from a hash avec timestamp
+                rmdir.sync(exportedChartInfo['parentDir']);
+
+                done();
+            });
+        }, 5000);
+    };
+
+    /**
+     * Supported export formats to test
+     * @type {Array}
+     */
+    var exportFormats = [
+        {extension : 'svg' , type : 'svg' , mediaType : 'image/svg+xml'},
+        {extension : 'png' , type : 'png' , mediaType : 'image/png'},
+        {extension : 'jpeg', type : 'jpeg', mediaType : 'image/jpeg'},
+        {extension : 'pdf' , type : 'pdf' , mediaType : 'application/pdf'}
+    ];
+
+    /**
+     * Run testExport over all supported formats
+     * @param  {object} format A format object as per exportFormats array
+     * @return {void}        Nothing
+     */
+    exportFormats.forEach(function(format){
+        testExport(format);
+    });
+
 });
